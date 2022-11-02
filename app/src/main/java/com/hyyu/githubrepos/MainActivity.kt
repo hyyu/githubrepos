b33aebcbf4c9482648f78ad9410f82ff0f610b38
danibo.a <danibo.a@sfeir.com>
2022-11-02 15:08:31 +0100

    Added a base for MainActivity

--- app/src/main/java/com/hyyu/githubrepos/MainActivity.kt
+++ app/src/main/java/com/hyyu/githubrepos/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hyyu.githubrepos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.hyyu.githubrepos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/hyyu/githubrepos/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.hyyu.githubrepos
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() { }
+
